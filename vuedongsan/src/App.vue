53c889ad2a060573296d17157c2a9c09d4f7b3c4
DevGangMin <fire0609@naver.com>
2025-01-20 18:01:58 +0900

    5일차까지 완료

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,39 +1,42 @@
 <template>
   <div>
+    <!-- 모달창 내용 -->
+    <div class="black-bg" v-if="ismodalopen == true">
+      <div class="white-bg">
+        <h4>상세페이지</h4>
+        <p>상세페이지내용임</p>
+        <button @click="ismodalopen = false">닫기</button>
+      </div>
+    </div>
+
+    <!-- 상단 메뉴 바 -->
     <div class="menu">
       <a v-for="menu in menus" :key="menu">{{ menu }}</a>
     </div>
-    <div>
-      <h4>{{products[0]}}</h4>
-      <p>50만원</p>
-      <button @click="reports[0]++">허위매물신고</button>
-      <span>신고수 : {{reports[0]}}</span>
+
+    <!-- 상품 목록 -->
+    <div v-for="(oneroom,i) in onerooms" :key="i">
+      <img :src="onerooms[i].image" class="room-img">
+      <h4 @click="isopenmodal = true">{{onerooms[i].title}}</h4>
+      <p>{{onerooms[i].price}}₩</p>
     </div>
-    <div>
-      <h4>{{products[1]}}</h4>
-      <p>60만원</p>
-      <button @click="reports[1]++">허위매물신고</button>
-      <span>신고수 : {{reports[1]}}</span>
-    </div>
-    <div>
-      <h4>{{products[2]}}</h4>
-      <p>70만원</p>
-      <button @click="reports[2]++">허위매물신고</button>
-      <span>신고수 : {{reports[2]}}</span>
-    </div>
+
   </div>
 </template>
 
 <script>
+import data from './assets/oneroom.js';
 export default {
   name : 'App',
   components : {
   },
   data(){
     return {
+      onerooms : data,
       menus : ['Home', 'Shop', 'About'],
-      products : ['역삼동원룸','천호동원룸','마포구원룸'],
       reports : [0, 0, 0],
+      products : ['역삼동원룸','천호동원룸','마포구원룸'],
+      ismodalopen : false,
     }
   }
 }
@@ -49,4 +52,20 @@ export default {
   color : white;
   padding : 10px;
 }
+body {
+  margin : 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height:100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+} 
 </style> 
